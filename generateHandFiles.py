08a4df3bfe772a994b702d2d0ea4fb32afe38af7
Jeff Miscione <jeffmiscione@gmail.com>
2016-12-11 14:41:03 -0500

    Update generateHandFiles.py

--- generateHandFiles.py
+++ generateHandFiles.py
@@ -9,14 +9,20 @@ class Application(tk.Frame):
         self.pack()
         self.createWidgets()
 
-    def editText(self, fileName, scale):
-        if(not(scale.isnumeric()) or scale == '0'):
-            scale = '1'
+    def editText(self, fileName, valThumb, valMiddle, valPalm):
+        if(not(valThumb.isnumeric()) or valThumb == '0'):
+            valThumb = '1'
+        if(not(valMiddle.isnumeric()) or valMiddle == '0'):
+            valMiddle = '1'
+        if(not(valPalm.isnumeric()) or valPalm == '0'):
+            valPalm = '1'
+
+        vals = (str(valThumb), str(valMiddle), str(valPalm))
 
         # Read file
         importedFile = open(fileName, "r")              # open the file in reading mode
         lineList = list(importedFile)                   # turn each line into an element of a list
-        lineList[0] = 'scale = ' + str(scale) + ';\n'   # change the first line (first element)in the list)
+        lineList[0] = 'thumb_length = %s; middle_length = %s; palm_width = %s;\n' % vals # change the first line (first element)in the list)
         importedFile.close()                            # close the file
 
         # Write file
@@ -25,31 +31,58 @@ class Application(tk.Frame):
             importedFile.write(str(line))          # write the line (element of the list) to the file
         importedFile.close()                    # close the file
 
-    def makeBoM(self, scale, reqTorque):
+    def makeBoM(self, valThumb, valMiddle, valPalm, reqTorque):
         # create blank file and list of lines
         BoMFile = open('BoM.csv', "w")
         lineList = []
 
         # create all lines for BoM
         lineList.append('Label, Description, Quantity' + '\n')
-        if reqTorque == 'High':
-            lineList.append('Motor 1, 25 oz-in, 1' + '\n')
-            lineList.append('Motor 2, 30 oz-in, 1' + '\n')
-        elif reqTorque == 'Medium':
-            lineList.append('Motor 1, 20 oz-in, 1' + '\n')
-            lineList.append('Motor 2, 25 oz-in, 1' + '\n')
-        elif reqTorque == 'Low':
-            lineList.append('Motor 1, 15 oz-in, 1' + '\n')
-            lineList.append('Motor 2, 20 oz-in, 1' + '\n')
 
-        lineList.append('Cable, 5 in, 1' + '\n')
-        lineList.append('Cable, 4.5 in, 1' + '\n')
-        lineList.append('Cable, 4 in, 1' + '\n')
-        lineList.append('Cable, 3.5 in, 1' + '\n')
-        lineList.append('Cable, 3 in, 1' + '\n')
-        lineList.append('Spring, 10 N/m Torsion, 14' + '\n')
+        # motors
+        # https://www.servocity.com/90-rpm-micro-gear-motor
+        if reqTorque == 'High':
+            lineList.append('Micro Gear Motor, 90 RPM 12V, 2' + '\n')
+        elif reqTorque == 'Medium':
+            lineList.append('Micro Gear Motor, 110 RPM 12V, 2' + '\n')
+        elif reqTorque == 'Low':
+            lineList.append('Micro Gear Motor, 130 RPM 12V, 2' + '\n')
+
+        # motor enclosure
+        # https://www.servocity.com/micro-gear-motor-enclosure
+        lineList.append('Motor Enclosure, Micro, 1' + '\n')     # 0.99 servocity
+
+        # Wire
+        # http://www.berkley-fishing.com/berkley-terminal-tackle-leaders-berkley-steelon/berkley-steelon-nylon-coated-wire/1285569.html#start=1
+        lineList.append('Nylon Coated Steel Wire (0.58mm), '+ str(float(valThumb) + 0.25*float(valPalm)) + 'mm, 1\n')    # thumb
+        lineList.append('Nylon Coated Steel Wire (0.58mm), '+ str(0.9*float(valMiddle) + 0.45*float(valPalm)) + 'mm, 1\n')  # index
+        lineList.append('Nylon Coated Steel Wire (0.58mm), '+ str(float(valMiddle) + 0.50*float(valPalm)) + 'mm, 1\n')    # middle
+        lineList.append('Nylon Coated Steel Wire (0.58mm), '+ str(0.85*float(valMiddle) + 0.45*float(valPalm)) + 'mm, 1\n')  # ring
+        lineList.append('Nylon Coated Steel Wire (0.58mm), '+ str(0.6*float(valMiddle) + 0.40*float(valPalm)) + 'mm, 1\n')    # pinky
+        lineList.append('Nylon Coated Steel Wire (0.58mm), '+ str(0.50*float(valPalm)) + ', 1\n')    # connector to motor
+
+        # https://www.mcmaster.com/#9271k74/=1594nh2
+        lineList.append('0.014" wire diam Spring, 0.075 in*lbs Torsion, 14' + '\n')
+
+        # nuts and bolts
         lineList.append('Joint Screw, 4-40, 14' + '\n')
         lineList.append('Joint Nut, 4-40, 14' + '\n')
+
+        # spool
+
+        # Battery and connectors
+        lineList.append('LIPO Battery, 1000mAh+ 3S 20C+, 1' + '\n') # $5.93 hobbyking
+        lineList.append('LIPO Battery Sleeve, Silicone, 1' + '\n')  # $0.01 hobbyking
+        lineList.append('XT60 Battery Connector, Male, 1' + '\n')  # $0.01 hobbyking
+        lineList.append('XT60 Battery Connector, Female, 1' + '\n')  # $0.01 hobbyking
+
+        # switches
+        lineList.append('DPDT Switch, Center Off, 1' + '\n')    # $5.93 hobbyking
+
+        # wire harness for other hand
+        lineList.append('Stranded Electrical Wire, ' + str(25*float(valPalm)) + 'mm 16 AWG, 4\n') # 4 because motor mounted on battery mounted on hand
+        lineList.append('Protective Wire Sleeve, ' + str(25*float(valPalm)) + 'mm length 3mm diameter, 1\n')
+        lineList.append('Heat Shrink Tubing, 6mm x 50mm, 2' + '\n')
 
         # write all lines to files
         for line in lineList:
@@ -61,30 +94,21 @@ class Application(tk.Frame):
         #filePath = "c:\\Users\Jeff\PycharmProjects\Biomedical_Robotics\SCADtoSTL.bat"
 
         # Read edit field data =========================================================
-        valThumb1 = self.efThumb1.get()
-        valThumb2 = self.efThumb2.get()
-        valIndex1 = self.efIndex1.get()
-        valIndex2 = self.efIndex2.get()
-        valIndex3 = self.efIndex3.get()
-        valMiddle1 = self.efMiddle1.get()
-        valMiddle2 = self.efMiddle2.get()
-        valMiddle3 = self.efMiddle3.get()
-        valRing1 = self.efRing1.get()
-        valRing2 = self.efRing2.get()
-        valRing3 = self.efRing3.get()
+        valThumb = self.efThumb.get()
+        valMiddle = self.efMiddle.get()
+        valPalm = self.efPalm.get()
 
         # Make SCAD files ===============================================================
-        self.editText('thumb1.scad', valThumb1)
-        self.editText('thumb2.scad', valThumb2)
-        self.editText('index1.scad', valIndex1)
-        self.editText('index2.scad', valIndex2)
-        self.editText('index3.scad', valIndex3)
-        self.editText('middle1.scad', valMiddle1)
-        self.editText('middle2.scad', valMiddle2)
-        self.editText('middle3.scad', valMiddle3)
-        self.editText('ring1.scad', valRing1)
-        self.editText('ring2.scad', valRing2)
-        self.editText('ring3.scad', valRing3)
+        # do for each part
+        self.editText('finger_tip.scad', valThumb, valMiddle, valPalm)
+        self.editText('middle_phalanx.scad', valThumb, valMiddle, valPalm)
+        self.editText('palm_1.scad', valThumb, valMiddle, valPalm)
+        #self.editText('Palm_Cover.scad', valThumb, valMiddle, valPalm)      # need to fix
+        #self.editText('Palm_Pulley.scad', valThumb, valMiddle, valPalm)     # need to fix
+        self.editText('proximal_phalanx.scad', valThumb, valMiddle, valPalm)
+        self.editText('proximal_thumb.scad', valThumb, valMiddle, valPalm)
+        self.editText('thumb_tip.scad', valThumb, valMiddle, valPalm)
+
 
         # Make STL files ================================================================
         # display 'creating stl files dialogue'
@@ -92,7 +116,7 @@ class Application(tk.Frame):
         self.update()
 
         # make the BoM
-        self.makeBoM(valThumb1,self.torque_box.get())       # edit what the scale value is going to be
+        self.makeBoM(valThumb, valMiddle, valPalm, self.torque_box.get())       # edit what the scale value is going to be
 
         # make the files
         p = subprocess.Popen(filePath)
@@ -103,77 +127,52 @@ class Application(tk.Frame):
         self.update()
 
     def createWidgets(self):
+        # thumb length, middle finger length, and palm width
+
         # Static Texts =======================================================================
         self.stSplash = tk.Label(self, text = "Prosthetic Hand STL Generator", font = ("Helvetica", 16))
 
-        self.stthumb1 = tk.Label(self, text = "Thumb 1 Length")
-        self.stthumb2 = tk.Label(self, text = "Thumb 2 Length")
-        self.stindex1 = tk.Label(self, text = "Index 1 Length")
-        self.stindex2 = tk.Label(self, text = "Index 2 Length")
-        self.stindex3 = tk.Label(self, text = "Index 3 Length")
-        self.stmiddle1 = tk.Label(self, text = "Middle 1 Length")
-        self.stmiddle2 = tk.Label(self, text = "Middle 2 Length")
-        self.stmiddle3 = tk.Label(self, text = "Middle 3 Length")
-        self.string1 = tk.Label(self, text = "Ring 1 Length")
-        self.string2 = tk.Label(self, text = "Ring 2 Length")
-        self.string3 = tk.Label(self, text = "Ring 3 Length")
-        self.sttorque = tk.Label(self, text = "Torque Required")
+        self.stThumb = tk.Label(self, text = "Thumb Length (mm)")
+        self.stMiddle = tk.Label(self, text = "Middle Finger Length (mm)")
+        self.stPalm = tk.Label(self, text = "Palm Width (mm)")
+        self.stTorque = tk.Label(self, text = "Torque Required")
 
         # Pushbuttons ========================================================================
         self.pbMakePartFiles = tk.Button(self, text = "Generate STL", command = self.makePartFiles)
         self.pbQuit = tk.Button(self, text = "QUIT", fg = "red", command = root.destroy)
 
         # Edit Fields ========================================================================
-        self.efThumb1 = tk.Entry(self)
-        self.efThumb2 = tk.Entry(self)
-        self.efIndex1 = tk.Entry(self)
-        self.efIndex2 = tk.Entry(self)
-        self.efIndex3 = tk.Entry(self)
-        self.efMiddle1 = tk.Entry(self)
-        self.efMiddle2 = tk.Entry(self)
-        self.efMiddle3 = tk.Entry(self)
-        self.efRing1 = tk.Entry(self)
-        self.efRing2 = tk.Entry(self)
-        self.efRing3 = tk.Entry(self)
+        self.efThumb = tk.Entry(self)
+        self.efMiddle = tk.Entry(self)
+        self.efPalm = tk.Entry(self)
+
+        # Image
+        self.handImage = tk.PhotoImage(file="handImage.gif")
+        self.imageBin = tk.Label(self,image=self.handImage)
 
         # Option Menu ========================================================================
         self.torque_box = ttk.Combobox(self, textvariable = StringVar(), state ='readonly')
         self.torque_box['values'] = ('Low', 'Medium', 'High')
 
         # Arrange Items ======================================================================
+        self.imageBin.grid(row = 0, column = 2, columnspan = 2, rowspan = 15)
+
         self.stSplash.grid(row = 0, column = 0, columnspan = 2, sticky ='E'+'W')
 
         self.pbMakePartFiles.grid(row = 1, column = 0, columnspan = 2, sticky ='E'+'W')
 
-        self.stthumb1.grid(row = 2, column = 0, sticky ='E'+'W')
-        self.stthumb2.grid(row = 3, column = 0, sticky ='E'+'W')
-        self.stindex1.grid(row = 4, column = 0, sticky ='E'+'W')
-        self.stindex2.grid(row = 5, column = 0, sticky ='E'+'W')
-        self.stindex3.grid(row = 6, column = 0, sticky ='E'+'W')
-        self.stmiddle1.grid(row = 7, column = 0, sticky ='E'+'W')
-        self.stmiddle2.grid(row = 8, column = 0, sticky ='E'+'W')
-        self.stmiddle3.grid(row = 9, column = 0, sticky ='E'+'W')
-        self.string1.grid(row = 10, column = 0, sticky ='E'+'W')
-        self.string2.grid(row = 11, column = 0, sticky ='E'+'W')
-        self.string3.grid(row = 12, column = 0, sticky ='E'+'W')
-        self.sttorque.grid(row = 13, column = 0, sticky ='E'+'W')
+        self.stThumb.grid(row = 2, column = 0, sticky ='E'+'W')
+        self.stMiddle.grid(row = 3, column = 0, sticky ='E'+'W')
+        self.stPalm.grid(row = 4, column = 0, sticky ='E'+'W')
+        self.stTorque.grid(row = 5, column = 0, sticky ='E'+'W')
 
-        self.efThumb1.grid(row = 2, column = 1, sticky ='E'+'W')
-        self.efThumb2.grid(row = 3, column = 1, sticky ='E'+'W')
-        self.efIndex1.grid(row = 4, column = 1, sticky ='E'+'W')
-        self.efIndex2.grid(row = 5, column = 1, sticky ='E'+'W')
-        self.efIndex3.grid(row = 6, column = 1, sticky ='E'+'W')
-        self.efMiddle1.grid(row = 7, column = 1, sticky ='E'+'W')
-        self.efMiddle2.grid(row = 8, column = 1, sticky ='E'+'W')
-        self.efMiddle3.grid(row = 9, column = 1, sticky ='E'+'W')
-        self.efRing1.grid(row = 10, column = 1, sticky ='E'+'W')
-        self.efRing2.grid(row = 11, column = 1, sticky ='E'+'W')
-        self.efRing3.grid(row = 12, column = 1, sticky ='E'+'W')
+        self.efThumb.grid(row = 2, column = 1, sticky ='E'+'W')
+        self.efMiddle.grid(row = 3, column = 1, sticky ='E'+'W')
+        self.efPalm.grid(row = 4, column = 1, sticky ='E'+'W')
 
-        self.torque_box.grid(row = 13, column = 1, sticky ='E'+'W')
+        self.torque_box.grid(row = 5, column = 1, sticky ='E'+'W')
 
-        self.pbQuit.grid(row = 14, column = 0, columnspan = 2, sticky ='E'+'W')
-
+        self.pbQuit.grid(row = 6, column = 0, columnspan = 2, sticky ='E'+'W')
 
 root = tk.Tk()
 root.title("Hand STL Generator")
